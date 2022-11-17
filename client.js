ca53d012a40ed8445ad2330afaad35d70cf89803
nathanbobell <67938436+nathanbobell@users.noreply.github.com>
2022-11-16 19:38:57 -0800

    Update client.js

--- client.js
+++ client.js
@@ -91,9 +91,27 @@ function handleArtistsResponse(){
         console.log(data);
       let genreList = [];
       data.items.forEach(artist => {
-      genreList = addToGenreDict(artist.genres, genreList);
+      arr1 = addToGenreDict(artist.genres, genreList);
     })
-      console.log(genreList);
+      var mf = 1;
+      var m = 0;
+      var item;
+      for (var i=0; i<arr1.length; i++)
+      {
+        for (var j=i; j<arr1.length; j++)
+        {
+                if (arr1[i] == arr1[j])
+                 m++;
+                if (mf<m)
+                {
+                  mf=m; 
+                  item = arr1[i];
+                }
+        }
+        m=0;
+      }
+    console.log(item+" ( " +mf +" times ) ") ;
+      console.log(arr1);
     }
     else if ( this.status == 401 ){
         refreshAccessToken()
